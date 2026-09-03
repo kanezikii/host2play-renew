e8d1ff757f26ff9086480f841b9902192a147685
kanezikii <peng320829@gmail.com>
2026-09-03 16:05:37 +0800

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -21,7 +21,7 @@ except ImportError:
 # ==============================================================================
 RENEW_URLS = [
     "https://host2play.gratis/server/renew?i=d3c08db5-a4bb-41fd-bd97-51c4a5195a85",
-    "https://host2play.gratis/server/renew?i=dd8034fb-4272-4257-beb7-a8202e62c75f",
+    "https://host2play.gratis/server/renew?i=d1f4fa35-295c-4ca8-af23-db9ce4c7d208",
 ]
 
 MAX_CAPTCHA = 3
